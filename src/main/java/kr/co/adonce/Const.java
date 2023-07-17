743a95001b77649c9cba320e2c5c7fb4557e83a7
adonce <jaehwanie20@gmail.com>
2023-07-17 22:44:59 +0900

    Enit Commit

---
+++ src/main/java/kr/co/adonce/Const.java
@@ -0,0 +1,21 @@
+package kr.co.adonce;
+
+/**
+ * 
+ *
+ * @since 2023. 7. 17.
+ * @author 김재환
+ */
+public class Const {
+	
+	/**
+	 * 테스트 (삭제예정)
+	 *
+	 * @since 2023. 7. 17.
+	 * @author 김재환
+	 */
+	public static final class NameTest {
+		public static final String NAME_TEST = "test_name";
+	}
+
+}
---
+++ src/main/java/kr/co/adonce/SbpApplication.java
@@ -0,0 +1,9 @@
+package kr.co.adonce;
+
+public class SbpApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(SbpApplication.class, args);
+	}
+
+}
