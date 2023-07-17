2a84ffc1cf99059a5a199b6cf2ae63f3bd45f834
adonce <jaehwanie20@gmail.com>
2023-07-17 23:14:11 +0900

    테스트 머지

--- src/main/java/kr/co/adonce/SbpApplication.java
+++ src/main/java/kr/co/adonce/SbpApplication.java
@@ -1,9 +1,17 @@
 package kr.co.adonce;
 
+/**
+ * 
+ *
+ * @since 2023. 7. 17.
+ * @author 김재환
+ */
 public class SbpApplication {
 
 	public static void main(String[] args) {
 //		SpringApplication.run(SbpApplication.class, args);
+		
+		
 		
 		
 		
